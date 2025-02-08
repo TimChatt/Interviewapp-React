176cc46980d374e30b30c426f389985db7b8a7f0
TimChattaway-HB <78868962+TimChattaway-HB@users.noreply.github.com>
2025-02-08 18:57:55 +0000

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 
 // Placeholder pages for now
 const Home = () => <h1>Home</h1>;
