f5238c6de4c7425bc1b44827e6cc60a4c057d888
TimChattaway-HB <78868962+TimChattaway-HB@users.noreply.github.com>
2025-02-08 19:02:59 +0000

    Update InsightsChart.jsx

--- components/InsightsChart.jsx
+++ components/InsightsChart.jsx
@@ -1,1 +1,40 @@
+import React from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+const InsightsChart = ({ data, xKey, yKeys, title }) => {
+  return (
+    <div className="insights-chart">
+      <h3>{title}</h3>
+      <ResponsiveContainer width="100%" height={400}>
+        <LineChart data={data} margin={{ top: 20, right: 30, left: 0, bottom: 0 }}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey={xKey} />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          {yKeys.map((key, index) => (
+            <Line
+              key={key}
+              type="monotone"
+              dataKey={key}
+              stroke={["#8884d8", "#82ca9d", "#ffc658"][index % 3]}
+              activeDot={{ r: 8 }}
+            />
+          ))}
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
+
+export default InsightsChart;
 
