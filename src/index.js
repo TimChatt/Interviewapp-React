138009c26023a6567b4a95e99a9cc6578eb20ae2
TimChattaway-HB <78868962+TimChattaway-HB@users.noreply.github.com>
2025-02-08 21:50:57 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css"; // Global styles
-import App from "./App";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Candidate from "./pages/Candidate";
+import Insights from "./pages/Insights";
+import Recommendations from "./pages/Recommendations";
+import Admin from "./pages/Admin";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/candidate" element={<Candidate />} />
+        <Route path="/insights" element={<Insights />} />
+        <Route path="/recommendations" element={<Recommendations />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
