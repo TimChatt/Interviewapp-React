3b107066bac99ef012ae20ee9d9e922ff9ed2d77
TimChattaway-HB <78868962+TimChattaway-HB@users.noreply.github.com>
2025-02-08 22:08:32 +0000

    Update NavigationBar.jsx

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,21 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./NavigationBar.css";
 
 const NavigationBar = () => {
   return (
-    <nav className="navbar">
-      <div className="navbar-container">
-        <Link to="/" className="navbar-logo">Dashboard</Link>
-        <div className="navbar-links">
-          <Link to="/candidate">Candidate</Link>
-          <Link to="/insights">Insights</Link>
-          <Link to="/recommendations">Recommendations</Link>
-          <Link to="/admin">Admin</Link>
-        </div>
-      </div>
+    <nav style={{ padding: "10px", backgroundColor: "#333", color: "#fff" }}>
+      <ul style={{ listStyle: "none", display: "flex", gap: "15px" }}>
+        <li>
+          <Link to="/" style={{ color: "#fff", textDecoration: "none" }}>Dashboard Overview</Link>
+        </li>
+        <li>
+          <Link to="/candidate" style={{ color: "#fff", textDecoration: "none" }}>Candidate</Link>
+        </li>
+        <li>
+          <Link to="/insights" style={{ color: "#fff", textDecoration: "none" }}>Insights</Link>
+        </li>
+        <li>
+          <Link to="/recommendations" style={{ color: "#fff", textDecoration: "none" }}>Recommendations</Link>
+        </li>
+        <li>
+          <Link to="/admin" style={{ color: "#fff", textDecoration: "none" }}>Admin</Link>
+        </li>
+      </ul>
     </nav>
   );
 };
 
 export default NavigationBar;
+
