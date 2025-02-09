bc43458502564b1183b63c72ca318d0b9257040e
TimChattaway-HB <78868962+TimChattaway-HB@users.noreply.github.com>
2025-02-09 14:10:01 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavigationBar from "./components/NavigationBar";
 import Home from "./pages/Home";
-import Candidate from "./pages/Candidate";
-import CandidateProfile from "./pages/CandidateProfile";
+import Candidate from "./pages/Candidate";  // The Candidate Table/List page
+import CandidateProfile from "./pages/CandidateProfile"; // Single candidate detail
 import Insights from "./pages/Insights";
 import Recommendations from "./pages/Recommendations";
 import Admin from "./pages/Admin";
