1f7cb09ec31449dd62dd60c9e61b268b1468bf68
TimChattaway-HB <78868962+TimChattaway-HB@users.noreply.github.com>
2025-02-09 13:23:08 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BerryProvider } from "react-berry-ui"; // Import the BerryProvider
 import NavigationBar from "./components/NavigationBar";
 import Home from "./pages/Home";
 import Candidate from "./pages/Candidate";  // The Candidate Table/List page
@@ -10,24 +11,26 @@ import Admin from "./pages/Admin";
 
 const App = () => {
   return (
-    <Router>
-      <NavigationBar />
-      <Routes>
-        {/* Home Page */}
-        <Route path="/" element={<Home />} />
+    <BerryProvider>
+      <Router>
+        <NavigationBar />
+        <Routes>
+          {/* Home Page */}
+          <Route path="/" element={<Home />} />
 
-        {/* List/Table of Candidates */}
-        <Route path="/candidates" element={<Candidate />} />
+          {/* List/Table of Candidates */}
+          <Route path="/candidates" element={<Candidate />} />
 
-        {/* Single Candidate Detail (parameterized route) */}
-        <Route path="/candidate/:candidateId" element={<CandidateProfile />} />
+          {/* Single Candidate Detail (parameterized route) */}
+          <Route path="/candidate/:candidateId" element={<CandidateProfile />} />
 
-        {/* Additional Pages */}
-        <Route path="/insights" element={<Insights />} />
-        <Route path="/recommendations" element={<Recommendations />} />
-        <Route path="/admin" element={<Admin />} />
-      </Routes>
-    </Router>
+          {/* Additional Pages */}
+          <Route path="/insights" element={<Insights />} />
+          <Route path="/recommendations" element={<Recommendations />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      </Router>
+    </BerryProvider>
   );
 };
 
