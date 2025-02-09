f82b24156e49c6b5418d6edad4d6ab99a43351bd
TimChattaway-HB <78868962+TimChattaway-HB@users.noreply.github.com>
2025-02-09 08:19:57 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import NavigationBar from "./components/NavigationBar"; // Correct import
+import NavigationBar from "./components/NavigationBar";
 import Home from "./pages/Home";
-import Candidate from "./pages/Candidate";
+import Candidate from "./pages/Candidate";  // We'll assume this is the "CandidateTable" view
+import CandidateProfile from "./pages/CandidateProfile"; // New file we create for single-candidate detail
 import Insights from "./pages/Insights";
 import Recommendations from "./pages/Recommendations";
 import Admin from "./pages/Admin";
@@ -10,10 +11,16 @@ import Admin from "./pages/Admin";
 const App = () => {
   return (
     <Router>
-      <NavigationBar /> {/* Render NavigationBar */}
+      <NavigationBar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/candidate" element={<Candidate />} />
+
+        {/* List/Table of candidates */}
+        <Route path="/candidates" element={<Candidate />} />
+
+        {/* Single candidate detail page, parameterized by ID */}
+        <Route path="/candidate/:candidateId" element={<CandidateProfile />} />
+
         <Route path="/insights" element={<Insights />} />
         <Route path="/recommendations" element={<Recommendations />} />
         <Route path="/admin" element={<Admin />} />
