a9a1a9053e126276bfb127e5ca120de4a32b3bd2
TimChattaway-HB <78868962+TimChattaway-HB@users.noreply.github.com>
2025-02-09 13:33:23 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,28 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { BerryProvider } from "react-berry-ui"; // Import the BerryProvider
 import NavigationBar from "./components/NavigationBar";
 import Home from "./pages/Home";
-import Candidate from "./pages/Candidate";  // The Candidate Table/List page
-import CandidateProfile from "./pages/CandidateProfile"; // Single candidate detail
+import Candidate from "./pages/Candidate";
+import CandidateProfile from "./pages/CandidateProfile";
 import Insights from "./pages/Insights";
 import Recommendations from "./pages/Recommendations";
 import Admin from "./pages/Admin";
 
 const App = () => {
   return (
-    <BerryProvider>
-      <Router>
-        <NavigationBar />
-        <Routes>
-          {/* Home Page */}
-          <Route path="/" element={<Home />} />
-
-          {/* List/Table of Candidates */}
-          <Route path="/candidates" element={<Candidate />} />
-
-          {/* Single Candidate Detail (parameterized route) */}
-          <Route path="/candidate/:candidateId" element={<CandidateProfile />} />
-
-          {/* Additional Pages */}
-          <Route path="/insights" element={<Insights />} />
-          <Route path="/recommendations" element={<Recommendations />} />
-          <Route path="/admin" element={<Admin />} />
-        </Routes>
-      </Router>
-    </BerryProvider>
+    <Router>
+      <NavigationBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/candidates" element={<Candidate />} />
+        <Route path="/candidate/:candidateId" element={<CandidateProfile />} />
+        <Route path="/insights" element={<Insights />} />
+        <Route path="/recommendations" element={<Recommendations />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </Router>
   );
 };
 
 export default App;
+
