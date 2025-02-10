d57bdccec435b3049c80ea39f9aa2e7935bc7c85
TimChatt <78868962+TimChatt@users.noreply.github.com>
2025-02-10 20:15:07 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 // src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './contexts/AuthContext';
-import PrivateRoute from './components/PrivateRoute';
-import NavigationBar from './components/NavigationBar';
-import Home from './pages/Home';
-import Candidate from './pages/Candidate';
-import CandidateProfile from './pages/CandidateProfile';
-import Insights from './pages/Insights';
-import Recommendations from './pages/Recommendations';
-import Admin from './pages/Admin';
-import Login from './pages/Login';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
+import PrivateRoute from "./components/PrivateRoute";
+import NavigationBar from "./components/NavigationBar";
+import Home from "./pages/Home";
+import Candidate from "./pages/Candidate";
+import CandidateProfile from "./pages/CandidateProfile";
+import Insights from "./pages/Insights";
+import Recommendations from "./pages/Recommendations";
+import Admin from "./pages/Admin";
+import Login from "./pages/Login";
+import AdminDashboard from "./pages/AdminDashboard";  // New admin dashboard page
 
 const App = () => {
   return (
@@ -27,6 +28,7 @@ const App = () => {
             <Route path="/insights" element={<Insights />} />
             <Route path="/recommendations" element={<Recommendations />} />
             <Route path="/admin" element={<Admin />} />
+            <Route path="/admin-dashboard" element={<AdminDashboard />} />
           </Route>
         </Routes>
       </Router>
@@ -35,3 +37,4 @@ const App = () => {
 };
 
 export default App;
+
