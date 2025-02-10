394528fb403050d38d8c08f6cedad4ed7b2e479e
TimChatt <78868962+TimChatt@users.noreply.github.com>
2025-02-10 21:41:34 +0000

    Update AuthContext.jsx

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../contexts/AuthContext";
-import "./Login.css";
 
 const Login = () => {
   const { login } = useContext(AuthContext);
