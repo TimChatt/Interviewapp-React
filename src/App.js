aa6ebd720c3123ec085752383f962a8267344056
TimChatt <78868962+TimChatt@users.noreply.github.com>
2025-02-10 20:54:21 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import PrivateRoute from "./components/PrivateRoute";
 import NavigationBar from "./components/NavigationBar";
@@ -10,18 +10,24 @@ import CandidateProfile from "./pages/CandidateProfile";
 import Insights from "./pages/Insights";
 import Recommendations from "./pages/Recommendations";
 import Admin from "./pages/Admin";
+import AdminDashboard from "./pages/AdminDashboard"; // New admin dashboard page
 import Login from "./pages/Login";
-import SignUp from "./pages/SignUp"; // Import the SignUp component
-import AdminDashboard from "./pages/AdminDashboard";  // New admin dashboard page
+import Signup from "./pages/Signup";
 
 const App = () => {
+  const location = useLocation();
+
+  // Define paths where the NavigationBar should be hidden
+  const hideNavBarPaths = ["/login", "/signup"];
+  const shouldHideNavBar = hideNavBarPaths.includes(location.pathname);
+
   return (
     <AuthProvider>
       <Router>
-        <NavigationBar />
+        {!shouldHideNavBar && <NavigationBar />}
         <Routes>
           <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />  {/* Added signup route */}
+          <Route path="/signup" element={<Signup />} />
           {/* Protected routes */}
           <Route element={<PrivateRoute />}>
             <Route path="/" element={<Home />} />
