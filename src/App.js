ca5db5c00909ce7494610273325836055982816d
TimChatt <78868962+TimChatt@users.noreply.github.com>
2025-02-10 18:12:08 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,37 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import NavigationBar from "./components/NavigationBar";
-import Home from "./pages/Home";
-import Candidate from "./pages/Candidate";  // The Candidate Table/List page
-import CandidateProfile from "./pages/CandidateProfile"; // Single candidate detail
-import Insights from "./pages/Insights";
-import InterviewerPlaybook from "./pages/InterviewerPlaybook";
-import Recommendations from "./pages/Recommendations";
-import Admin from "./pages/Admin";
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+import PrivateRoute from './components/PrivateRoute';
+import NavigationBar from './components/NavigationBar';
+import Home from './pages/Home';
+import Candidate from './pages/Candidate';
+import CandidateProfile from './pages/CandidateProfile';
+import Insights from './pages/Insights';
+import Recommendations from './pages/Recommendations';
+import Admin from './pages/Admin';
+import Login from './pages/Login';
 
 const App = () => {
   return (
-    <Router>
-      <NavigationBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/candidates" element={<Candidate />} />
-        <Route path="/candidate/:candidateId" element={<CandidateProfile />} />
-        <Route path="/insights" element={<Insights />} />
-        <Route path="/recommendations" element={<Recommendations />} />
-        <Route path="/interviewer-playbook" element={<InterviewerPlaybook />} />
-        <Route path="/admin" element={<Admin />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <NavigationBar />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          {/* Protected routes */}
+          <Route element={<PrivateRoute />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/candidates" element={<Candidate />} />
+            <Route path="/candidate/:candidateId" element={<CandidateProfile />} />
+            <Route path="/insights" element={<Insights />} />
+            <Route path="/recommendations" element={<Recommendations />} />
+            <Route path="/admin" element={<Admin />} />
+          </Route>
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 };
 
 export default App;
-
