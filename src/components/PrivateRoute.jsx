bc181f041c60ef636eb8ea414cbc061e7d20eed5
TimChatt <78868962+TimChatt@users.noreply.github.com>
2025-02-10 18:11:08 +0000

    Create PrivateRoute.jsx

---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,12 @@
+// src/components/PrivateRoute.jsx
+import React, { useContext } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import { AuthContext } from '../contexts/AuthContext';
+
+const PrivateRoute = () => {
+  const { user, loading } = useContext(AuthContext);
+  if (loading) return <div>Loading...</div>;
+  return user ? <Outlet /> : <Navigate to="/login" />;
+};
+
+export default PrivateRoute;
