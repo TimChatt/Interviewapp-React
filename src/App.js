d42b556941a2c0825ab8e42dc54e70211cc61d24
TimChatt <78868962+TimChatt@users.noreply.github.com>
2025-02-12 14:41:03 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,57 +1,39 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Home from "./pages/Home";
 import Candidate from "./pages/Candidate";
 import CandidateProfile from "./pages/CandidateProfile";
-import Insights from "./pages/Insights";
-import Recommendations from "./pages/Recommendations";
 import Admin from "./pages/Admin";
 import AdminDashboard from "./pages/AdminDashboard";
 import CompetencyFrameworkPlanner from "./pages/CompetencyFrameworkPlanner";
 import Login from "./pages/Login";
-import Signup from "./pages/SignUp";
+import SignUp from "./pages/SignUp";
 import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <Router>
       <div className="app-container">
-        {/* Sidebar navigation */}
         <Sidebar />
-
-        {/* Main content area */}
         <div className="main-content">
           <Routes>
-            {/* Home page */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
             <Route path="/" element={<Home />} />
-
-            {/* Candidate routes */}
-            <Route path="/candidate/search" element={<Candidate />} />
-            <Route path="/candidate/profile" element={<CandidateProfile />} />
-
-            {/* Insights and recommendations */}
-            <Route path="/insights" element={<Insights />} />
-            <Route path="/recommendations" element={<Recommendations />} />
-
-            {/* Admin routes */}
+            <Route path="/candidates" element={<Candidate />} />
+            <Route path="/candidate/:candidateId" element={<CandidateProfile />} />
             <Route path="/admin" element={<Admin />} />
             <Route path="/admin/dashboard" element={<AdminDashboard />} />
-
-            {/* Competency Framework Planner */}
             <Route
-              path="/competency-framework-planner"
+              path="/admin/competency-framework"
               element={<CompetencyFrameworkPlanner />}
             />
-
-            {/* Authentication */}
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
           </Routes>
         </div>
       </div>
     </Router>
   );
-}
+};
 
 export default App;
-
