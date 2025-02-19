e15705c6fd6705411e12462fd1b7467d18f020f1
TimChatt <78868962+TimChatt@users.noreply.github.com>
2025-02-19 18:18:02 +0000

    Update SignUp.jsx

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./SignUp.css";
+import '../styles.css';
 
 const SignUp = () => {
   const [username, setUsername] = useState("");
