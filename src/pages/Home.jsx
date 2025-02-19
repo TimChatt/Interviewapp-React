1a2d2ee5877b9f8441fe326492dbbf5a1034add5
TimChatt <78868962+TimChatt@users.noreply.github.com>
2025-02-19 18:14:05 +0000

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 // src/pages/Home.jsx
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./Home.css";
+import '../styles.css';
 import ashbyMockData from "../mockdata/ashbyMockData.json";
 
 /**
