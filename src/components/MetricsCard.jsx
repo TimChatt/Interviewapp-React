efacd532a5b67986810bace4deb34b3e620d5060
TimChatt <78868962+TimChatt@users.noreply.github.com>
2025-02-19 18:11:15 +0000

    Update MetricsCard.jsx

--- src/components/MetricsCard.jsx
+++ src/components/MetricsCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./MetricsCard.css"; // Import CSS for styling the metrics card
+import '../styles.css';
 
 const MetricsCard = ({ title, value, icon }) => {
   return (
