bd23b692e79b1aab17f884e025abd882d4a0d936
Thomas Woodhams <39730099+gingerninja88@users.noreply.github.com>
2025-06-24 17:18:45 +0100

    Update server.js

--- client/server.js
+++ client/server.js
@@ -4,11 +4,11 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Serve static files from CRA build folder
-app.use(express.static(path.join(__dirname, "build")));
+// Serve static files from CRA build folder inside /client
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
 app.listen(port, () => {
