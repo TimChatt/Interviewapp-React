9814cb016cbefb5be21837a970671de8ab1b42dd
Thomas Woodhams <39730099+gingerninja88@users.noreply.github.com>
2025-06-24 10:44:19 +0100

    Update main.py

--- server/main.py
+++ server/main.py
@@ -1806,6 +1806,6 @@ if __name__ == "__main__":
         fetch_and_store_data(session)
     finally:
         session.close()
-	    
+
     logging.info("🚀 Starting the FastAPI server...")
-    uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
+    uvicorn.run("server.main:app", host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
